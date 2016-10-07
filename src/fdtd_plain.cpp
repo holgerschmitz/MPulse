b742432afe27573242527f6d80732bf64f4b6c3d
Holger Schmitz <holger@notjustphysics.com>
2016-10-07 13:51:34 +0100

    Moved some old files Integration of simple FDTD algorithm complete

--- src/fdtd_plain.cpp
+++ src/fdtd_plain.cpp
@@ -7,25 +7,17 @@
 
 #include "fdtd_plain.hpp"
 
-void FDTD_Plain::initStorage(Storage *storage_)
-{  
-  storage = storage_;
-  
-  pEx = storage->addGrid("Ex");
-  pEy = storage->addGrid("Ey");
-  pEz = storage->addGrid("Ez");
+#include <schnek/grid.hpp>
 
-  pBx = storage->addGrid("Bx");
-  pBy = storage->addGrid("By");
-  pBz = storage->addGrid("Bz");
-  
-  storage->addToGroup("E", "Ex");
-  storage->addToGroup("E", "Ey");
-  storage->addToGroup("E", "Ez");
+void FDTD_Plain::init()
+{
+  retrieveData("Ex", pEx);
+  retrieveData("Ey", pEy);
+  retrieveData("Ez", pEz);
 
-  storage->addToGroup("B", "Bx");
-  storage->addToGroup("B", "By");
-  storage->addToGroup("B", "Bz");
+  retrieveData("Bx", pBx);
+  retrieveData("By", pBy);
+  retrieveData("Bz", pBz);
 }
 
 void FDTD_Plain::stepSchemeInit(double dt)
@@ -41,63 +33,64 @@ void FDTD_Plain::stepScheme(double dt)
 
 void FDTD_Plain::stepD(double dt)
 {
-  DataGrid &Ex = *pEx;
-  DataGrid &Ey = *pEy;
-  DataGrid &Ez = *pEz;
+  Field &Ex = *pEx;
+  Field &Ey = *pEy;
+  Field &Ez = *pEz;
   
-  DataGrid &Bx = *pBx;
-  DataGrid &By = *pBy;
-  DataGrid &Bz = *pBz;
+  Field &Bx = *pBx;
+  Field &By = *pBy;
+  Field &Bz = *pBz;
   
-  GridIndex low = storage->getLow();
-  GridIndex high = storage->getHigh();
+  Index low = Ex.getInnerLo();
+  Index high = Ex.getInnerHi();
 
-  double dx = storage->getDx();
-  double dy = storage->getDy();
-  double dz = storage->getDz();
+  Vector dx = MPulse::getDx();
 
   for (int i=low[0]+1; i<=high[0]; ++i)
     for (int j=low[1]+1; j<=high[1]; ++j)
       for (int k=low[2]+1; k<=high[2]; ++k)
   {
     Ex(i,j,k) = Ex(i,j,k) 
-      + dt*(
-          (Bz(i,j,k) - Bz(i,j-1,k))/dy
-        - (By(i,j,k) - By(i,j,k-1))/dz
+      + dt*clight2*(
+          (Bz(i,j,k) - Bz(i,j-1,k))/dx[1]
+        - (By(i,j,k) - By(i,j,k-1))/dx[2]
       );
       
     Ey(i,j,k) = Ey(i,j,k) 
-      + dt*(
-          (Bx(i,j,k) - Bx(i,j,k-1))/dz
-        - (Bz(i,j,k) - Bz(i-1,j,k))/dx
+      + dt*clight2*(
+          (Bx(i,j,k) - Bx(i,j,k-1))/dx[2]
+        - (Bz(i,j,k) - Bz(i-1,j,k))/dx[0]
       );
  
     Ez(i,j,k) = Ez(i,j,k) 
-      + dt*(
-          (By(i,j,k) - By(i-1,j,k))/dx
-        - (Bx(i,j,k) - Bx(i,j-1,k))/dy
+      + dt*clight2*(
+          (By(i,j,k) - By(i-1,j,k))/dx[0]
+        - (Bx(i,j,k) - Bx(i,j-1,k))/dx[1]
       );
   }
-  storage->applyBoundary("E");
+
+  schnek::DomainSubdivision<Field> &sub = MPulse::getSubdivision();
+
+  sub.exchange(Ex);
+  sub.exchange(Ey);
+  sub.exchange(Ez);
 }
 
 
 void FDTD_Plain::stepB(double dt)
 {
-  DataGrid &Ex = *pEx;
-  DataGrid &Ey = *pEy;
-  DataGrid &Ez = *pEz;
+  Field &Ex = *pEx;
+  Field &Ey = *pEy;
+  Field &Ez = *pEz;
   
-  DataGrid &Bx = *pBx;
-  DataGrid &By = *pBy;
-  DataGrid &Bz = *pBz;
+  Field &Bx = *pBx;
+  Field &By = *pBy;
+  Field &Bz = *pBz;
 
-  GridIndex low = storage->getLow();
-  GridIndex high = storage->getHigh();
+  Index low = storage->getLow();
+  Index high = storage->getHigh();
 
-  double dx = storage->getDx();
-  double dy = storage->getDy();
-  double dz = storage->getDz();
+  Vector dx = storage->getDx();
 
   for (int i=low[0]; i<high[0]; ++i)
     for (int j=low[1]; j<high[1]; ++j)
@@ -105,21 +98,26 @@ void FDTD_Plain::stepB(double dt)
   {
     Bx(i,j,k) = Bx(i,j,k) 
       + dt*(
-          (Ey(i,j,k+1) - Ey(i,j,k))/dz
-        - (Ez(i,j+1,k) - Ez(i,j,k))/dy
+          (Ey(i,j,k+1) - Ey(i,j,k))/dx[2]
+        - (Ez(i,j+1,k) - Ez(i,j,k))/dx[1]
       );
 
     By(i,j,k) = By(i,j,k) 
       + dt*(
-          (Ez(i+1,j,k) - Ez(i,j,k))/dx
-        - (Ex(i,j,k+1) - Ex(i,j,k))/dz
+          (Ez(i+1,j,k) - Ez(i,j,k))/dx[0]
+        - (Ex(i,j,k+1) - Ex(i,j,k))/dx[2]
       );
 
     Bz(i,j,k) = Bz(i,j,k) 
       + dt*( 
-          (Ex(i,j+1,k) - Ex(i,j,k))/dx
-        - (Ey(i+1,j,k) - Ey(i,j,k))/dx
+          (Ex(i,j+1,k) - Ex(i,j,k))/dx[1]
+        - (Ey(i+1,j,k) - Ey(i,j,k))/dx[0]
       );
   }
-  storage->applyBoundary("B");
+
+  schnek::DomainSubdivision<Field> &sub = MPulse::getSubdivision();
+
+  sub.exchange(Bx);
+  sub.exchange(By);
+  sub.exchange(Bz);
 }
