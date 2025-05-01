a2aa3a22db15269e1237b209fa2d2cbae812c46f
terencerl <terence.lobo@stfc.ac.uk>
2025-05-01 14:04:47 +0100

    updated code to reflect func name change & test new logic

--- src/plasmacurrent.cpp
+++ src/plasmacurrent.cpp
@@ -16,6 +16,7 @@ std::chrono::duration<double> total_time1(0);
 std::chrono::duration<double> total_time2(0);
 std::chrono::duration<double> total_time3(0);
 std::chrono::duration<double> total_time4(0);
+std::chrono::duration<double> total_time5(0);
 int iteration_count = 0;
 
 
@@ -129,6 +130,7 @@ void PlasmaCurrent::stepScheme(double dt)
   Grid Jx1, Jy1, Jz1;
   Field Ex1, Ey1, Ez1, Rho1;
 
+  // Creating temporary vars
   Jx1.resize(Jx.getLo(), Jx.getHi());
   Jy1.resize(Jy.getLo(), Jy.getHi());
   Jz1.resize(Jz.getLo(), Jz.getHi());
@@ -136,15 +138,13 @@ void PlasmaCurrent::stepScheme(double dt)
   Ey1.resize(Ey.getLo(), Ey.getHi(), Ey.getDomain(), Ey.getStagger(), Ey.getghostCells());
   Ez1.resize(Ez.getLo(), Ez.getHi(), Ez.getDomain(), Ez.getStagger(), Ez.getghostCells());
   Rho1.resize(Rho.getLo(), Rho.getHi(), Rho.getDomain(), Rho.getStagger(), Rho.getghostCells());
-
+  
   for (auto &pos : range) {
     Jx1[pos] = Jx[pos]; Jy1[pos] = Jy[pos]; Jz1[pos] = Jz[pos];
     Ex1[pos] = Ex[pos]; Ey1[pos] = Ey[pos]; Ez1[pos] = Ez[pos];
     Rho1[pos] = Rho[pos];
   }
-
-  // Jx1 = Jx; Jy1 = Jy; Jz1 = Jz;
-  // Ex1 = Ex; Ey1 = Ey; Ez1 = Ez; Rho1 = Rho;
+  // Creating temporary vars
 
   if (Jx(rn, rn) != 0) std::cout << "(Before 1) Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
   if (Jy(rn, rn) != 0) std::cout << "(Before 1) Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
@@ -172,19 +172,21 @@ void PlasmaCurrent::stepScheme(double dt)
   if (Jy(rn, rn) != 0) std::cout << "(After 1)  Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
   if (Jz(rn, rn) != 0) std::cout << "(After 1)  Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
 
+  // Re-assignment
   for (auto &pos : range) {
     Jx[pos] = Jx1[pos];
     Jy[pos] = Jy1[pos];
     Jz[pos] = Jz1[pos];
   }
+  // Re-assignment
   
   std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
   total_time1 += duration;
-
+  
   if (Jx(rn, rn) != 0) std::cout << "(Before 2) Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
   if (Jy(rn, rn) != 0) std::cout << "(Before 2) Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
   if (Jz(rn, rn) != 0) std::cout << "(Before 2) Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
-
+  
   // Case 2
   start = std::chrono::high_resolution_clock::now();
   
@@ -200,54 +202,29 @@ void PlasmaCurrent::stepScheme(double dt)
   }
   
   end = std::chrono::high_resolution_clock::now();
-
+  
   if (Jx(rn, rn) != 0) std::cout << "(After 2)  Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
   if (Jy(rn, rn) != 0) std::cout << "(After 2)  Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
   if (Jz(rn, rn) != 0) std::cout << "(After 2)  Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
-
+  
   duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
   total_time2 += duration;
-
-  // Test
-  // Grid g1_test;
-
-  // g1_test = Jx;
   
-  // Kokkos::parallel_for("plasmacurrent_2D",
-  // Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
-  //     {low[0], low[1]}, 
-  //     {high[0], high[1]}
-  //   ),
-  //   KOKKOS_LAMBDA (const int i, const int j) {
-  //     // Create index for the current position
-  //     typename Grid::IndexType pos;
-  //     pos[0] = i;
-  //     pos[1] = j;
-      
-  //     double jx = Jx[pos];
-  //     // double jy = Jy_1[pos];
-  //     // double jz = Jz_1[pos];
-  //     // double rho = Rho[pos];
-
-  //     Jx.set(pos, (jx*gdtn + emdt*Ex[pos])/gdtd);
-  //     // Jy_1.set(pos, (jy*gdtn + emdt*Ey_1[pos]*rho)/gdtd);
-  //     // Jz_1.set(pos, (jz*gdtn + emdt*Ez_1[pos]*rho)/gdtd);
-  //   }
-  // );
-
-  // Case 3
+  // Re-assignment
   for (auto &pos : range) {
     Jx[pos] = Jx1[pos];
     Jy[pos] = Jy1[pos];
     Jz[pos] = Jz1[pos];
   }
-
+  // Re-assignment
+  
   if (Jx(rn, rn) != 0) std::cout << "(Before 3) Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
   if (Jy(rn, rn) != 0) std::cout << "(Before 3) Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
   if (Jz(rn, rn) != 0) std::cout << "(Before 3) Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
-
+  
+  // Case 3
   start = std::chrono::high_resolution_clock::now();
-
+  
   Jx.parallel_func(low, high, SCHNEK_DEVICE_LAMBDA(const auto& pos) {
     double jx = Jx[pos];
     double jy = Jy[pos];
@@ -260,198 +237,90 @@ void PlasmaCurrent::stepScheme(double dt)
   });
   
   end = std::chrono::high_resolution_clock::now();
-
+  
   if (Jx(rn, rn) != 0) std::cout << "(After 3)  Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
   if (Jy(rn, rn) != 0) std::cout << "(After 3)  Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
   if (Jz(rn, rn) != 0) std::cout << "(After 3)  Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
   
   duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
   total_time3 += duration;
-
-  // Test
-
-  // // Testing
-  // Grid2d grid1(low2D, high2D);
-  // Grid2d grid2(low2D, high2D);
-
-  // // g(low2D[0], high2D[1]) = 1.0;
-  // fill_kokkos_grid(grid1, 0.5);
-
-  // // grid.t
-  // // error: no type named ‘storage_iterator’ in ‘class schnek::KokkosGridStorage<double, 2>’
-  // // typedef typename StoragePolicy::storage_iterator Iterator;
-  // // grid1 = 0.5;
-
-  // grid2 = grid1;
-
-  // // auto dims = Jx.getDims();
-  // dims = Jx.getDims();
-
-  // // std::cout << "Jx Dims : " << dims[0] << ", " << dims[1] << std::endl;
-
-  // double grid1Sum = grid1.reduce(std::plus<double>(), 0.0);
-  // double grid2Sum = grid2.reduce(std::plus<double>(), 0.0);
   
-  // // std::cout << "Sum of grid1 elements: " << grid1Sum << std::endl;
-  // // std::cout << "Sum of grid2 elements: " << grid2Sum << std::endl;
-  
-  // auto grid1Size = grid1.getSize();
-  // // std::cout << "Size of grid1 : " << grid1Size << std::endl;
-  
-  // typedef schnek::Field<double, 2, HuertoGridChecker, schnek::KokkosDefaultGridStorage> Field2N;
-  // typedef schnek::Grid<double, 2, HuertoGridChecker, schnek::KokkosDefaultGridStorage> Grid2N;
-  // // int n = 100;
-  
-  // Field2N f1;
+  // Re-assignment
+  for (auto &pos : range) {
+    Jx[pos] = Jx1[pos];
+    Jy[pos] = Jy1[pos];
+    Jz[pos] = Jz1[pos];
+  }
+  // Re-assignment
 
-  // auto f1dims = f1.getDims();
-  // auto f1Size = f1.getSize();
-  // double f1Sum = f1.reduce(std::plus<double>(), 0.0);
+  if (Jx(rn, rn) != 0) std::cout << "(Before 4) Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
+  if (Jy(rn, rn) != 0) std::cout << "(Before 4) Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
+  if (Jz(rn, rn) != 0) std::cout << "(Before 4) Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
 
-  // // std::cout << "f1 Dims : " << f1dims[0] << ", " << f1dims[1] << std::endl;
-  // // std::cout << "f1 Size : " << f1Size << std::endl;
-  // // std::cout << "f1 Sum  : " << f1Sum << std::endl;
+  // Case 4
+  start = std::chrono::high_resolution_clock::now();
 
-  // Grid2N Jx_1, Jy_1, Jz_1;
-  // Field2N Ex_1, Ey_1, Ez_1;
-  
-  // Jx_1.resize(Jx.getLo(), Jx.getHi());
-  // Jy_1.resize(Jy.getLo(), Jy.getHi());
-  // Jz_1.resize(Jz.getLo(), Jz.getHi());
-  
-  // for (auto &pos : range) {
-  //   Jx_1[pos] = Jx[pos];
-  //   Jy_1[pos] = Jy[pos];
-  //   Jz_1[pos] = Jz[pos];
-
-  //   // if (Jx[pos] != 0) std::cout << "Jx("<<pos[0]<<" ,"<<pos[1]<<") : "<<Jx_1[pos]<<" - "<<Jx[pos]<<std::endl;
-  //   // if (Jy[pos] != 0) std::cout << "Jy("<<pos[0]<<" ,"<<pos[1]<<") : "<<Jy_1[pos]<<" - "<<Jy[pos]<<std::endl;
-  //   // if (Jz[pos] != 0) std::cout << "Jz("<<pos[0]<<" ,"<<pos[1]<<") : "<<Jz_1[pos]<<" - "<<Jz[pos]<<std::endl;
-  // }
-
-  // Ex_1.resize(Ex.getLo(), Ex.getHi(), Ex.getDomain(), Ex.getStagger(), Ex.getghostCells());
-  // Ey_1.resize(Ey.getLo(), Ey.getHi(), Ey.getDomain(), Ey.getStagger(), Ey.getghostCells());
-  // Ez_1.resize(Ez.getLo(), Ez.getHi(), Ez.getDomain(), Ez.getStagger(), Ez.getghostCells());
-  
-  // for (auto &pos : range) {
-  //   Ex_1[pos] = Ex[pos];
-  //   Ey_1[pos] = Ey[pos];
-  //   Ez_1[pos] = Ez[pos];
-
-  //   // if (Ex[pos] != 0) std::cout << "Ex("<<pos[0]<<" ,"<<pos[1]<<") : "<<Ex_1[pos]<<" - "<<Ex[pos]<<std::endl;
-  //   // if (Ey[pos] != 0) std::cout << "Ey("<<pos[0]<<" ,"<<pos[1]<<") : "<<Ey_1[pos]<<" - "<<Ey[pos]<<std::endl;
-  //   // if (Ez[pos] != 0) std::cout << "Ez("<<pos[0]<<" ,"<<pos[1]<<") : "<<Ez_1[pos]<<" - "<<Ez[pos]<<std::endl;
-  // }
-
-  // Field2N Rho_1;
-
-  // Rho_1.resize(
-  //   Rho.getLo(), 
-  //   Rho.getHi(),     
-  //   Rho.getDomain(), 
-  //   Rho.getStagger(),
-  //   Rho.getghostCells()
-  //   // 0
-  // );
-
-  // // std::cout << "Rho ghostCells : "<<Rho.getghostCells()<<std::endl;
-
-  // for (auto &pos : range) {
-  //   Rho_1[pos] = Rho[pos];
-  //   // std::cout << "Rho("<<pos[0]<<" ,"<<pos[1]<<") : "<<Rho_1[pos]<<" - "<<Rho[pos]<<std::endl;
-  // }
-
-  // // Case 3
-  // start = std::chrono::high_resolution_clock::now();
-
-  // // WORKS using redefined variables
-  // Kokkos::parallel_for("plasmacurrent_2D",
-  //   Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
-  //     {low[0], low[1]}, 
-  //     {high[0], high[1]}
-  //   ),
-  //   KOKKOS_LAMBDA (const int i, const int j) {
-  //     // Create index for the current position
-  //     typename Field2N::IndexType pos;
-  //     pos[0] = i;
-  //     pos[1] = j;
-      
-  //     // Access field values
-  //     double jx = Jx_1[pos];
-  //     double jy = Jy_1[pos];
-  //     double jz = Jz_1[pos];
-  //     double rho = Rho_1[pos];
-      
-  //     // THESE ASSIGNMENT OPERATIONS DO NOT WORK - error
-  //     // error occurs because when using [] inside a KOKKOS_LAMBDA, you don't get a reference that can be modified
-  //     // it is designed to execute on devices (not the host) - so references to the host are not allowed
-  //     // Jx_1[pos] = (jx*gdtn + emdt*Ex_1[pos]*rho)/gdtd;
-  //     // Jy_1[pos] = (jy*gdtn + emdt*Ey_1[pos]*rho)/gdtd;
-  //     // Jz_1[pos] = (jz*gdtn + emdt*Ez_1[pos]*rho)/gdtd;
-
-  //     Jx_1.set(pos, (jx*gdtn + emdt*Ex_1[pos]*rho)/gdtd);
-  //     Jy_1.set(pos, (jy*gdtn + emdt*Ey_1[pos]*rho)/gdtd);
-  //     Jz_1.set(pos, (jz*gdtn + emdt*Ez_1[pos]*rho)/gdtd);
-  //   }
-  // );
-
-  // Kokkos::fence();
-
-  // end = std::chrono::high_resolution_clock::now();
-  
-  // duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-  // total_time3 += duration;
-
-  // // testing
-  // auto JxSize = Jx.getSize();
-  // // std::cout << "Jx Size : " << JxSize << std::endl;
-
-  // // throws error
-  // // double JxSum = Jx.reduce(std::plus<double>(), 0.0);
-  // // std::cout << "Sum of Jx elements: " << JxSum << std::endl;
-
-  // double Jz1Sum = Jz_1.reduce(std::plus<double>(), 0.0);
-  // // std::cout << "Sum of Jz_1 elements: " << Jz1Sum << std::endl;
-
-  // // Case 4
-  // start = std::chrono::high_resolution_clock::now();
-
-  // Jx_1.parallel_func(low, high, [&](const auto& pos) {
-  //   double jx = Jx_1[pos];
-  //   double jy = Jy_1[pos];
-  //   double jz = Jz_1[pos];
-  //   double rho = Rho_1[pos];
+  schnek::kokkos_utils::parallel_kokkos_parallel_for<Field>(low, high, SCHNEK_DEVICE_LAMBDA(const auto& pos) {
+    double jx = Jx[pos];
+    double jy = Jy[pos];
+    double jz = Jz[pos];  
+    double rho = Rho[pos];
     
-  //   Jx_1.set(pos, (jx*gdtn + emdt*Ex_1[pos]*rho)/gdtd);
-  //   Jy_1.set(pos, (jy*gdtn + emdt*Ey_1[pos]*rho)/gdtd);
-  //   Jz_1.set(pos, (jz*gdtn + emdt*Ez_1[pos]*rho)/gdtd);
-  // });
+    Jx.set(pos, (jx*gdtn + emdt*Ex[pos]*rho)/gdtd);
+    Jy.set(pos, (jy*gdtn + emdt*Ey[pos]*rho)/gdtd);
+    Jz.set(pos, (jz*gdtn + emdt*Ez[pos]*rho)/gdtd);
+  });
 
-  // // Jx.parallel_func(low, high, [&](const auto& pos) {
-  // //   // Access field values
-  // //   double jx = Jx[pos];
-  // //   double jy = Jy[pos];
-  // //   double jz = Jz[pos];
-  // //   double rho = Rho[pos];
-    
-  // //   // Calculate new values using set method
-  // //   Jx.set(pos, (jx*gdtn + emdt*Ex[pos]*rho)/gdtd);
-  // //   Jy.set(pos, (jy*gdtn + emdt*Ey[pos]*rho)/gdtd);
-  // //   Jz.set(pos, (jz*gdtn + emdt*Ez[pos]*rho)/gdtd);
-  // // });
+  end = std::chrono::high_resolution_clock::now();
 
-  // Kokkos::fence();
-
-  // end = std::chrono::high_resolution_clock::now();
+  if (Jx(rn, rn) != 0) std::cout << "(After 4)  Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
+  if (Jy(rn, rn) != 0) std::cout << "(After 4)  Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
+  if (Jz(rn, rn) != 0) std::cout << "(After 4)  Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
   
-  // duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-  // total_time4 += duration;
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  total_time4 += duration;
 
+  // Re-assignment
+  for (auto &pos : range) {
+    Jx[pos] = Jx1[pos];
+    Jy[pos] = Jy1[pos];
+    Jz[pos] = Jz1[pos];
+  }
+  // Re-assignment
+
+  if (Jx(rn, rn) != 0) std::cout << "(Before 5) Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
+  if (Jy(rn, rn) != 0) std::cout << "(Before 5) Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
+  if (Jz(rn, rn) != 0) std::cout << "(Before 5) Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
+
+  // Case 5
+  start = std::chrono::high_resolution_clock::now();
+
+  schnek::kokkos_utils::parallel_kokkos_iteration<Field>(low, high, SCHNEK_DEVICE_LAMBDA(const auto& pos) {
+    double jx = Jx[pos];
+    double jy = Jy[pos];
+    double jz = Jz[pos];  
+    double rho = Rho[pos];
+    
+    Jx.set(pos, (jx*gdtn + emdt*Ex[pos]*rho)/gdtd);s
+    Jy.set(pos, (jy*gdtn + emdt*Ey[pos]*rho)/gdtd);
+    Jz.set(pos, (jz*gdtn + emdt*Ez[pos]*rho)/gdtd);
+  });
+
+  end = std::chrono::high_resolution_clock::now();
+
+  if (Jx(rn, rn) != 0) std::cout << "(After 5)  Jx("<<rn<<", "<<rn<<") : "<<Jx(rn, rn)<<" - "<<Jx1(rn, rn)<<std::endl;
+  if (Jy(rn, rn) != 0) std::cout << "(After 5)  Jy("<<rn<<", "<<rn<<") : "<<Jy(rn, rn)<<" - "<<Jy1(rn, rn)<<std::endl;
+  if (Jz(rn, rn) != 0) std::cout << "(After 5)  Jz("<<rn<<", "<<rn<<") : "<<Jz(rn, rn)<<" - "<<Jz1(rn, rn)<<std::endl;
+
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  total_time5 += duration;
+  
   if (iteration_count % 10 == 0){
     std::cout << "Total execution time (1): " << total_time1.count() << " ms" << std::endl;
     std::cout << "Total execution time (2): " << total_time2.count() << " ms" << std::endl;
     std::cout << "Total execution time (3): " << total_time3.count() << " ms" << std::endl;
     std::cout << "Total execution time (4): " << total_time4.count() << " ms" << std::endl;
+    std::cout << "Total execution time (5): " << total_time5.count() << " ms" << std::endl;
     iteration_count = 0;
   }
 
